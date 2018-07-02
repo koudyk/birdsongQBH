52dfbe0de474731d98894483d807e05dc2569f20
KendraOudyk <kendra.oudyk@gmail.com>
2018-07-02 15:34:53 -0400

    downloading audio - doing it again with resampling and with the same search parameter options as the website

--- b8using_downloadXC_function.m
+++ b8using_downloadXC_function.m
@@ -7,7 +7,7 @@ exhard='F:\0.birdsongQBH\audio'; %external harddrive
 addpath(genpath(exhard))
 addpath(genpath(laptop))
 
-names=sort({...
+basic=sort({...
     'northern cardinal'...
     'black-capped chickadee'...
     'mourning dove'...
@@ -16,44 +16,81 @@ names=sort({...
     'sora'...
     'common yellowthroat'...
     'prairie warbler'...
+    'black-throated blue warbler'...
+    'veery'
     });
 
-
 dwnldDir=exhard;
 wgetDir='C:\Users\User\Downloads';
-type='song';
-%quality='B'; % greater than B (i.e., A)
-quality='C'; % greater than C (i.e., A or B)
+
+advanced = {'type:song' 'q>:C' };
 maxQuantity=Inf;
-nums=1:length(names);
-
-[ recDet, recMeta ] = downloadXC( wgetDir, dwnldDir,names,type,quality,maxQuantity,nums); %,maxNum );
-%% adding another species
-
-clc,clear,%close all, clear sound
-laptop='C:\Users\User\Documents\MATLAB\Projects\birdsongQBH';
-exhard='F:\0.birdsongQBH\audio'; %external harddrive
-addpath(genpath(exhard))
-addpath(genpath(laptop))
-
-names={'Veery'};
-
-dwnldDir=exhard;
-wgetDir='C:\Users\User\Downloads';
-type='song';
-%quality='B'; % greater than B (i.e., A)
-quality='C'; % greater than C (i.e., A or B)
-maxQuantity=Inf;
-
-cd(exhard)
-list_spc=dir('spc*');
-Nspec=length(list_spc); % number of species already downloaded
-nums=Nspec+1:Nspec+length(names); % species number(s) for the new species
-
-[ recDet, recMeta ] = downloadXC( wgetDir, dwnldDir,names,type,quality,maxQuantity,nums); %,maxNum );
+nums=1:length(basic);
+targetFs=44100;
+tic
+[ meta ] = downloadXC(  wgetDir, dwnldDir,basic,nums,advanced,targetFs,maxQuantity);
+toc % Elapsed time is 6153.665652 seconds.
 
 
-% cd(dwnldDir)
-% save(['b8_output_audioData_' datestr(now,'yyyy-mm-dd')],'recDet','recMeta')
-
-
+%%
+% clc,clear,%close all, clear sound
+% laptop='C:\Users\User\Documents\MATLAB\Projects\birdsongQBH';
+% exhard='F:\0.birdsongQBH\audio'; %external harddrive
+% addpath(genpath(exhard))
+% addpath(genpath(laptop))
+% 
+% names=sort({...
+%     'northern cardinal'...
+%     'black-capped chickadee'...
+%     'mourning dove'...
+%     'white-throated sparrow'...
+%     'red-eyed vireo'...
+%     'sora'...
+%     'common yellowthroat'...
+%     'prairie warbler'...
+%     'black-throated blue warbler'...
+%     'veery'
+%     });
+% 
+% dwnldDir=exhard;
+% wgetDir='C:\Users\User\Downloads';
+% type='song';
+% %quality='B'; % greater than B (i.e., A)
+% quality='C'; % greater than C (i.e., A or B)
+% maxQuantity=Inf;
+% nums=1:length(names);
+% targetFs=44100;
+% 
+% tic
+% [ meta ] = downloadXC( wgetDir, dwnldDir,names,type,quality,maxQuantity,nums,targetFs); %,maxNum );
+% toc % Elapsed time is 5114.166943 seconds.
+% %% adding another species
+% 
+% clc,clear,%close all, clear sound
+% laptop='C:\Users\User\Documents\MATLAB\Projects\birdsongQBH';
+% exhard='F:\0.birdsongQBH\audio'; %external harddrive
+% addpath(genpath(exhard))
+% addpath(genpath(laptop))
+% 
+% %names={'Veery'};
+% names={'Black-throated blue warbler'};
+% 
+% dwnldDir=exhard;
+% wgetDir='C:\Users\User\Downloads';
+% type='song';
+% %quality='B'; % greater than B (i.e., A)
+% quality='C'; % greater than C (i.e., A or B)
+% maxQuantity=Inf;
+% 
+% cd(exhard)
+% list_spc=dir('spc*');
+% Nspec=length(list_spc); % number of species already downloaded
+% nums=Nspec+1:Nspec+length(names); % species number(s) for the new species
+% 
+% [ recDet, recMeta ] = downloadXC( wgetDir, dwnldDir,names,type,quality,maxQuantity,nums); %,maxNum );
+% 
+% 
+% % cd(dwnldDir)
+% % save(['b8_output_audioData_' datestr(now,'yyyy-mm-dd')],'recDet','recMeta')
+% 
+% 
