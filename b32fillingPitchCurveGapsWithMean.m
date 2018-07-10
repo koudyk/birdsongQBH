80b9926da297d985a335355941360a5e09c4a402
KendraOudyk <kendra.oudyk@gmail.com>
2018-07-10 17:10:17 -0400

    filling pitch curve gaps with mean

---
+++ b32fillingPitchCurveGapsWithMean.m
@@ -0,0 +1,31 @@
+
+
+
+%clc,clear,close all, clear sound
+laptop='C:\Users\User\Documents\MATLAB\Projects\birdsongQBH';
+exhard='F:\0.birdsongQBH\audio'; %external harddrive
+addpath(genpath(exhard))
+addpath(genpath(laptop))
+cd(exhard)
+
+list_spec = dir('pitchCurves_excerpts_cents_spc*_NXC-Nexcerpt.mat');
+Nspec=10;
+
+for nspec=1:Nspec
+    load(list_spec(nspec).name)
+    clear pitchCurves_filled
+    NXC=length(pitchCurves); % number of xeno-canto recordings
+    for nXC=1:NXC % Xeno-Canto file
+        fprintf('\n spec %d, XC ID %d / %d',nspec,nXC,NXC)
+        
+        Nexcerpt=length(pitchCurves{nXC});
+        for nexcerpt=1:Nexcerpt % pitch curve of clean excerpt in the XC file
+            excerpt = pitchCurves{nXC}{nexcerpt};
+            excerpt(isnan(excerpt))=nanmean(excerpt);
+            pitchCurves_filled{nXC}{nexcerpt}=excerpt;
+        end
+    end
+    speciesFile=sprintf('pitchCurves_filledGaps_cents_spc%02d_NXC-Nexcerpt',nspec);
+    save(speciesFile,'pitchCurves_filled','nspec','xcID','wsize_sec','ssize_sec',...
+       'hop_pwin','fs','fmin_hz','fmax_hz','tooShort','-v7.3')
+end
